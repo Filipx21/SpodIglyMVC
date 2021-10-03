88efc5e65525c687480a7d918c341ec77a7da164
Filipx21 <filip.zajac2277@gmail.com>
2021-10-03 16:36:06 +0200

    StoreContext

---
+++ SpodIglyMVC/DAL/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using SpodIglyMVC.Models;
+using System.Data.Entity;
+
+namespace SpodIglyMVC.DAL
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext() : base("StoreContext") { }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+    }
+}
