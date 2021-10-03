4462b141129b9a0dcf02eeec1b2eaf078bddb189
Filipx21 <filip.zajac2277@gmail.com>
2021-10-03 16:06:22 +0200

    Models

---
+++ SpodIglyMVC/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SpodIglyMVC.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public string Descripton { get; set; }
+        public string IconFilename { get; set; }
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
---
+++ SpodIglyMVC/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpodIglyMVC.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string CodeAndCity { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+        public DateTime DateCreated { get; set; }
+        public OrderState OrderState { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+
+    public enum OrderState
+    {
+        New,
+        Shipped
+    }
+}
