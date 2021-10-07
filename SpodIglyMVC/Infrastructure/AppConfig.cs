e71e100bdb34b73735eafc454b914dac60da4f09
Filipx21 <filip.zajac2277@gmail.com>
2021-10-07 18:51:09 +0200

    AppConfig for helper

---
+++ SpodIglyMVC/Infrastructure/AppConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+
+namespace SpodIglyMVC.Infrastructure
+{
+    public class AppConfig
+    {
+        private static string _genreIconsFolderRelative = ConfigurationManager.AppSettings["GenreIconsFolder"];
+        private static string _photosFolderRelative = ConfigurationManager.AppSettings["PhotosFolder"];
+
+        public static string GenreIconsFolderRelative { 
+            get {
+                return _genreIconsFolderRelative;
+            } 
+        }
+
+        public static string PhotosFolderRelative
+        {
+            get
+            {
+                return _genreIconsFolderRelative;
+            }
+        }
+    }
+}
