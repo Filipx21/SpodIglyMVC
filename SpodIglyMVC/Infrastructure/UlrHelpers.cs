b1f10d842733a4685e8a5fd4fb58a6af3d9b826a
Filipx21 <filip.zajac2277@gmail.com>
2021-10-07 18:58:18 +0200

    Helpers

--- SpodIglyMVC/Infrastructure/UlrHelpers.cs
+++ SpodIglyMVC/Infrastructure/UlrHelpers.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
+﻿using System.IO;
 using System.Web.Mvc;
 
 namespace SpodIglyMVC.Infrastructure
@@ -26,5 +22,6 @@ namespace SpodIglyMVC.Infrastructure
 
             return absolutePath;
         }
+
     }
 }
