adcec5983e7d8784fdb7754b6214709cc761eaf6
Filipx21 <filip.zajac2277@gmail.com>
2021-10-07 18:34:50 +0200

    controller

--- SpodIglyMVC/Controllers/HomeController.cs
+++ SpodIglyMVC/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SpodIglyMVC.DAL;
+using SpodIglyMVC.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,28 @@ namespace SpodIglyMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private StoreContext db = new StoreContext();
         public ActionResult Index()
         {
-            return View();
+            var genres = db.Genres;
+            var newArrivals = db.Albums
+                .Where(x => x.IsHidden == false)
+                .OrderByDescending(a => a.DateAdded)
+                .Take(3)
+                .ToList();
+            var bestseller = db.Albums
+                .Where(x => x.IsHidden == false && x.IsBestseller)
+                .OrderBy(x => Guid.NewGuid())
+                .Take(3)
+                .ToList();
+            var vm = new HomeViewModel()
+            {
+                Bestsellers = bestseller,
+                Genres = genres,
+                NewArrivals = newArrivals
+            };
+
+            return View(vm);
         }
 
         public ActionResult StaticContent(string viewname) 
