f8613a26e32af96b3e5c0b9d73a1200694ac47b5
Filipx21 <filip.zajac2277@gmail.com>
2021-10-07 18:57:48 +0200

    AlbumCoverPath Helper

--- SpodIglyMVC/Infrastructure/UlrHelpers.cs
+++ SpodIglyMVC/Infrastructure/UlrHelpers.cs
@@ -14,6 +14,16 @@ namespace SpodIglyMVC.Infrastructure
             var genreIconFolder = AppConfig.GenreIconsFolderRelative;
             var path = Path.Combine(genreIconFolder, genreIconFilename);
             var absolutePath = helper.Content(path);
+
+            return absolutePath;
+        }
+
+        public static string AlbumCoverPath(this UrlHelper helper, string albumFilename)
+        {
+            var albumCoverFolder = AppConfig.PhotosFolderRelative;
+            var path = Path.Combine(albumCoverFolder, albumFilename);
+            var absolutePath = helper.Content(path);
+
             return absolutePath;
         }
     }
