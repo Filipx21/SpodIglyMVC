855e9dd3950e747d1d61e32c1d82af4ec8a1ad68
Filipx21 <filip.zajac2277@gmail.com>
2021-10-10 18:17:42 +0200

    ProductDetailsDynamicNodeProvider

---
+++ SpodIglyMVC/Infrastructure/ProductDetailsDynamicNodeProvider.cs
@@ -0,0 +1,30 @@
+﻿using MvcSiteMapProvider;
+using SpodIglyMVC.DAL;
+using SpodIglyMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpodIglyMVC.Infrastructure
+{
+    public class ProductDetailsDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private StoreContext db = new StoreContext();
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var nodes = new List<DynamicNode>();
+
+            foreach(Album album in db.Albums)
+            {
+                DynamicNode dynamicNode = new DynamicNode();
+                dynamicNode.Title = album.AlbumTitle;
+                dynamicNode.Key = "Album_" + album.AlbumId;
+                dynamicNode.ParentKey = "Genre_" + album.GenreId;
+                dynamicNode.RouteValues.Add("id", album.AlbumId);
+                nodes.Add(dynamicNode);
+            }
+            return nodes;
+        }
+    }
+}
