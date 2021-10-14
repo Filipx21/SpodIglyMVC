3f498dd24c59f22215d262feba0889a8f8be3117
Filipx21 <filip.zajac2277@gmail.com>
2021-10-14 14:23:21 +0200

    Bundle

--- SpodIglyMVC/App_Start/BundleConfig.cs
+++ SpodIglyMVC/App_Start/BundleConfig.cs
@@ -13,6 +13,10 @@ namespace SpodIglyMVC
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryAndJqueryUi").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-{version}.js"));
+
             // Użyj wersji deweloperskiej biblioteki Modernizr do nauki i opracowywania rozwiązań. Następnie, kiedy wszystko będzie
             // gotowe do produkcji, użyj narzędzia do kompilowania ze strony https://modernizr.com, aby wybrać wyłącznie potrzebne testy.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
