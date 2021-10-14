ec8fe13ea50c98ba031e66259f16c640444dcda5
Filipx21 <filip.zajac2277@gmail.com>
2021-10-14 18:44:18 +0200

    SessionManager

---
+++ SpodIglyMVC/Infrastructure/SessionManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web;
+using System.Web.SessionState;
+
+namespace SpodIglyMVC.Infrastructure
+{
+    public class SessionManager : ISessionManager
+    {
+        private HttpSessionState _session;
+
+        public SessionManager()
+        {
+            _session = HttpContext.Current.Session;
+        }
+
+        public void Set<T>(string name, T value)
+        {
+            _session[name] = value;
+        }
+
+
+        public T Get<T>(string key)
+        {
+            return (T)_session[key];
+        }
+
+        public T TryGet<T>(string key)
+        {
+            try
+            {
+                return (T)_session[key];
+            }
+            catch (NullReferenceException)
+            {
+                return default(T);
+            }
+        }
+
+        public T Get<T>(string key, Func<T> createDefault)
+        {
+            T retval;
+
+            if (_session[key] != null && _session[key].GetType() == typeof(T))
+            {
+                retval = (T)_session[key];
+            }
+            else
+            {
+                retval = createDefault();
+                _session[key] = retval;
+            }
+
+            return retval;
+        }
+
+        public void Abandon()
+        {
+            _session.Abandon();
+        }
+    }
+}
