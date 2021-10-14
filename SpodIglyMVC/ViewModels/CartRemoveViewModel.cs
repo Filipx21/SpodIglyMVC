abb6f98b6ac4f1d2e9ada281d4962d3eee46b5ab
Filipx21 <filip.zajac2277@gmail.com>
2021-10-14 19:48:29 +0200

    CartRemoveViewModel

---
+++ SpodIglyMVC/ViewModels/CartRemoveViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SpodIglyMVC.ViewModels
+{
+    public class CartRemoveViewModel
+    {
+        public decimal CartTotal{ get; set; }
+        public int CartItemsCount { get; set; }
+        public int RemovedItemCount{ get; set; }
+        public int RemoveItemId { get; set; }
+    }
+}
