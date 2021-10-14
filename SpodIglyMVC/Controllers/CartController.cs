7a3b7804d0ae7cda32119532c04c232fc1d58e7f
Filipx21 <filip.zajac2277@gmail.com>
2021-10-14 19:22:58 +0200

    AddToCart

--- SpodIglyMVC/Controllers/CartController.cs
+++ SpodIglyMVC/Controllers/CartController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SpodIglyMVC.DAL;
+using SpodIglyMVC.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,25 @@ namespace SpodIglyMVC.Controllers
 {
     public class CartController : Controller
     {
-        // GET: Cart
+        private ISessionManager sessionManager { get; set; }
+        private ShoppingCartManager shoppingCartManager;
+        private StoreContext db;
+
+        public CartController()
+        {
+            this.sessionManager = new SessionManager();
+            this.shoppingCartManager = new ShoppingCartManager(sessionManager, db);
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            shoppingCartManager.AddToCart(id);
+            return RedirectToAction("Index");
         }
     }
 }
