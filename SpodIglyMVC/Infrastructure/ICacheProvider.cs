e6dc8750c93651fcbd472338a3b0667967fdf0dc
Filipx21 <filip.zajac2277@gmail.com>
2021-10-14 18:42:52 +0200

    ISessionManager

--- SpodIglyMVC/Infrastructure/ICacheProvider.cs
+++ SpodIglyMVC/Infrastructure/ICacheProvider.cs
@@ -3,7 +3,6 @@ namespace SpodIglyMVC.Infrastructure
 {
     public interface ICacheProvider
     {
-
         object Get(string key);
         void Set(string key, object data, int cacheTime);
         bool IsSet(string key);
