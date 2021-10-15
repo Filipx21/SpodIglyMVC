47b2f793c398dc5d9dcb8a0fee6641c7c20c917b
Filipx21 <filip.zajac2277@gmail.com>
2021-10-16 01:00:46 +0200

    LoginViewModel

---
+++ SpodIglyMVC/ViewModels/AccountViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SpodIglyMVC.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Musisz wprowadzić email")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Musisz wprowadzic hasło.")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
