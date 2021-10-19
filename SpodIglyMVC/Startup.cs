4aee1871bf799cedc34f7393c5a730747e19c88c
Filipx21 <filip.zajac2277@gmail.com>
2021-10-19 19:52:53 +0200

    Startup Owin

---
+++ SpodIglyMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+
+namespace SpodIglyMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+           
+        }
+    }
+}
