cbcb60b06f117cd1e3c02aada1e68472a1bbb7e4
Filipx21 <filip.zajac2277@gmail.com>
2021-10-19 19:26:08 +0200

    UserData

--- SpodIglyMVC/App_Start/IdentityConfig.cs
+++ SpodIglyMVC/App_Start/IdentityConfig.cs
@@ -14,8 +14,6 @@ using SpodIglyMVC.DAL;
 
 namespace SpodIglyMVC.Models
 {
-    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
-
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
