7089901f5dfd311962ea204d3c7f9da6bb1abb90
Filipx21 <filip.zajac2277@gmail.com>
2021-10-19 19:31:19 +0200

    AccountController

--- SpodIglyMVC/Controllers/AccountController.cs
+++ SpodIglyMVC/Controllers/AccountController.cs
@@ -1,15 +1,32 @@
-﻿using SpodIglyMVC.ViewModels;
+﻿using SpodIglyMVC.Models;
+using SpodIglyMVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace SpodIglyMVC.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
+        public AccountController() {}
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
         public ActionResult Login(string returnUrl)
         {
             return View();
