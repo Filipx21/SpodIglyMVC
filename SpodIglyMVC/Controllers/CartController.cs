7c200cebb922db204701bc1931884665f768b640
Filipx21 <filip.zajac2277@gmail.com>
2021-10-28 19:32:31 +0200

    add ApplicationUserManager

--- SpodIglyMVC/Controllers/CartController.cs
+++ SpodIglyMVC/Controllers/CartController.cs
@@ -17,11 +17,24 @@ namespace SpodIglyMVC.Controllers
         private ISessionManager sessionManager { get; set; }
         private ShoppingCartManager shoppingCartManager;
         private StoreContext db = new StoreContext();
+        private ApplicationUserManager _userManager;
 
         public CartController()
         {
             this.sessionManager = new SessionManager();
             this.shoppingCartManager = new ShoppingCartManager(sessionManager, db);
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
         }
 
         public ActionResult Index()
